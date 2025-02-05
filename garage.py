3e8b23a4b98746d077a4cb549abbe3bfad4d9070
Randy Le <le.hy.randy@gmail.com>
2025-02-04 21:36:12 -0600

    added checkbox authenticator in index.html, and css stylesheet, basic template not working yet

--- garage.py
+++ garage.py
@@ -1,28 +1,35 @@
-from flask import Flask, render_template
-import RPi.GPIO as GPIO
+from flask import Flask, render_template, request
+# import RPi.GPIO as GPIO
 import time
 
 app = Flask(__name__)
 
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BOARD)
-RELAY_PIN = 7
-GPIO.setup(RELAY_PIN, GPIO.OUT)
-GPIO.output(RELAY_PIN, True)
+# GPIO.setwarnings(False)
+# GPIO.setmode(GPIO.BOARD)
+# RELAY_PIN = 7
+# GPIO.setup(RELAY_PIN, GPIO.OUT)
+# GPIO.output(RELAY_PIN, True)
+# open = False
+
+PATTERN = {(0, 1), (0, 3), (1, 1), (1, 4), (3, 0)}
 
 def activate_garage_door():
-    GPIO.output(RELAY_PIN, False)
+    # GPIO.output(RELAY_PIN, False)
     time.sleep(0.8)
-    GPIO.output(RELAY_PIN, True)
+    # GPIO.output(RELAY_PIN, True)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    garage_door_button = '''<h1>Garage Door</h1>
-    <form action="/open" method="POST">
-        <button type="submit">Activate Garage Door</button>
-    </form>
-    '''
-    return garage_door_button
+    # garage_door_button = '''<h1>Garage Door</h1>
+    # <form action="/open" method="POST">
+    #     <button type="submit">Activate Garage Door</button>
+    # </form>
+    # '''
+    if request.method == 'POST':
+        selected = set()
+
+
+    return render_template('index.html')
 
 @app.route('/open', methods=['POST'])
 def open_garage():
