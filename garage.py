d0f00aeba3c7503e5cfbc36b5e537673c2996d91
Randy Le <le.hy.randy@gmail.com>
2025-02-05 20:25:08 -0600

    debugged index function to accept input correctly

--- garage.py
+++ garage.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 # import RPi.GPIO as GPIO
 import time
 
@@ -9,16 +9,16 @@ app = Flask(__name__)
 # RELAY_PIN = 7
 # GPIO.setup(RELAY_PIN, GPIO.OUT)
 # GPIO.output(RELAY_PIN, True)
-# open = False
 
-PATTERN = {(0, 1), (0, 3), (1, 1), (1, 4), (3, 0)}
+PATTERN = {(0, 1), (0, 3), (1, 0), (3, 0), (3, 1)}
 
 def activate_garage_door():
     # GPIO.output(RELAY_PIN, False)
+    print("opening")
     time.sleep(0.8)
     # GPIO.output(RELAY_PIN, True)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         selected = set()
@@ -26,20 +26,11 @@ def index():
         for key in request.form.keys():
             row, col = map(int, key.split('_'))
             selected.add((row, col))
-        
+
         if selected == PATTERN:
-            open_garage()
-        else:
-            return "❌ Incorrect Pattern! Try Again.", 403
-
-
+            activate_garage_door()
+            
     return render_template('index.html')
-
-@app.route('/open', methods=['POST'])
-def open_garage():
-    activate_garage_door()
-    message = '<h1>Garage Door Activated</h1><a href="/">Back</a>'
-    return message
 
 if __name__ == '__main__':
     try:
